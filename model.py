d9841ae301fbf5eeedfd3b9d625387f048f709cd
prapti1112 <praptipatil01@gmail.com>
2024-10-07 08:38:43 +0530

    🔨 Added masking to decoder

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 """Transformer model following 'Attention is All We Need' by Vaswani et al."""
+import logging
 import tensorflow as tf
 from logzero import logger
 from tensorflow.keras import layers
@@ -45,7 +46,7 @@ class Encoder(tf.keras.Model):
         x += positional_encoding
 
         for encoder_blocker_counter in range(h):
-            x += self.mha(self.query(x), self.key(x), self.value(x))
+            x += self.mha(query=self.query(x), key=self.key(x), value=self.value(x))
             x = self.layer_norm(x)
 
             x_fc = layers.Dense(4*self.d, activation="relu")(x)       # set as per formula: 4*d_model
@@ -57,14 +58,15 @@ class Encoder(tf.keras.Model):
 
 
 class Decoder(tf.keras.Model):
-    def __init__(self, mha_num_heads = 2, key_dim=3, seq_len=100):
+    def __init__(self, mha_num_heads = 2, key_dim=3):
         super(Decoder, self).__init__()
         self.d = mha_num_heads * key_dim
 
         self.query = layers.Dense(self.d, name="query_weights")
         self.key = layers.Dense(self.d, name="key_weights")
         self.value =layers.Dense(self.d, name="value_weights")
-        self.mha = layers.MultiHeadAttention(num_heads = mha_num_heads, key_dim=key_dim)
+        self.mha1 = layers.MultiHeadAttention(num_heads = mha_num_heads, key_dim=key_dim)
+        self.mha2 = layers.MultiHeadAttention(num_heads = mha_num_heads, key_dim=key_dim)
 
         self.layer_norm = layers.LayerNormalization(axis=-1, epsilon=1e-6)
     
@@ -77,10 +79,10 @@ class Decoder(tf.keras.Model):
         positional_encoding = Positional_Encoder(x, self.d)
         dec_out_prev += positional_encoding
 
-        dec_out_prev += self.mha(self.query(dec_out_prev), self.key(dec_out_prev), self.value(dec_out_prev))
+        dec_out_prev += self.mha1(query=self.query(dec_out_prev), key=self.key(dec_out_prev), value=self.value(dec_out_prev))
         dec_out_prev = self.layer_norm(dec_out_prev)
 
-        dec_out_prev += self.mha(self.query(x), self.key(dec_out_prev), self.value(dec_out_prev))
+        dec_out_prev += self.mha2(query=self.query(dec_out_prev), key=self.key(x), value=self.value(x), use_causal_mask=True)
         x = self.layer_norm(dec_out_prev)
 
         x_fc = layers.Dense(4*self.d, activation="relu")(x)       # set as per formula: 4*d_model
@@ -96,26 +98,27 @@ class Transformer(tf.keras.Model):
         self.decoder = decoder
     
     def __call__(self, x):
-        enc_out = encoder(x)
-        dec_out = decoder(enc_out)
+        enc_out = self.encoder(x)
+        dec_out = self.decoder(enc_out)
 
         final_output = layers.Dense(1)(dec_out)
         return final_output
 
 
-# if __name__ == "__main__":
-#     import numpy as np
+if __name__ == "__main__":
+    import numpy as np
 
-#     input_dim = 6
-#     output_dim = 1
-#     hidden_dim = 128
-#     seq_len = 100
-#     batch_size = 1
-#     dummpy_input = np.random.randn(batch_size, seq_len, input_dim)
-#     logger.debug(dummpy_input.shape)
+    input_dim = 6
+    output_dim = 1
+    hidden_dim = 128
+    seq_len = 100
+    batch_size = 1
+    dummpy_input = np.random.randn(batch_size, seq_len, input_dim)
+    logger.debug(dummpy_input.shape)
 
-#     encoder = Encoder()
-#     decoder = Decoder()
+    encoder = Encoder()
+    decoder = Decoder()
 
-#     model = Transformer(encoder, decoder)
-#     out = model(dummpy_input)
+    model = Transformer(encoder, decoder)
+    out = model(dummpy_input)
+    logger.debug(out.shape)
